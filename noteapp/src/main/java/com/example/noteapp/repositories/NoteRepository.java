4c10b24a170361877bfaae8616f36cf01bf85f13
KatePril <ket14pril14@gmail.com>
2024-11-25 21:18:05 +0200

    Add repositories

---
+++ noteapp/src/main/java/com/example/noteapp/repositories/NoteRepository.java
@@ -0,0 +1,9 @@
+package com.example.noteapp.repositories;
+
+import com.example.noteapp.entities.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+}
