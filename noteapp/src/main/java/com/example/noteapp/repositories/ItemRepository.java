d2bb0f9d056794aecde02630b90bbb526a711d82
KatePril <ket14pril14@gmail.com>
2024-11-29 19:27:34 +0200

    Update ItemService

--- noteapp/src/main/java/com/example/noteapp/repositories/ItemRepository.java
+++ noteapp/src/main/java/com/example/noteapp/repositories/ItemRepository.java
@@ -1,9 +1,12 @@
 package com.example.noteapp.repositories;
 
 import com.example.noteapp.entities.Item;
+import com.example.noteapp.entities.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    Iterable<Item> findItemsByNote(Note note);
+    Item findItemByItemId(int id);
 }
--- noteapp/src/main/java/com/example/noteapp/services/ItemService.java
+++ noteapp/src/main/java/com/example/noteapp/services/ItemService.java
@@ -1,15 +1,29 @@
 package com.example.noteapp.services;
 
+import com.example.noteapp.entities.Item;
+import com.example.noteapp.entities.Note;
 import com.example.noteapp.repositories.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ItemService {
-    private ItemRepository itemRepository;
+    private final ItemRepository itemRepository;
 
     @Autowired
     public ItemService(ItemRepository itemRepository) {
         this.itemRepository = itemRepository;
+    }
+
+    public Iterable<Item> getItemsByNote(Note note) {
+        return itemRepository.findItemsByNote(note);
+    }
+
+    public Item getItemById(int id) {
+        return itemRepository.findItemByItemId(id);
+    }
+
+    public Item createOrUpdateItem(Item item) {
+        return itemRepository.save(item);
     }
 }
