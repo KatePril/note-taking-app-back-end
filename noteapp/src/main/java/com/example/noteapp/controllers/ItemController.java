c11b69b0682bc54bbe309c85845f598f1a64948b
KatePril <ket14pril14@gmail.com>
2024-12-02 17:36:21 +0200

    Update ItemController

--- noteapp/src/main/java/com/example/noteapp/controllers/ItemController.java
+++ noteapp/src/main/java/com/example/noteapp/controllers/ItemController.java
@@ -1,17 +1,50 @@
 package com.example.noteapp.controllers;
 
-import com.example.noteapp.repositories.ItemRepository;
+import com.example.noteapp.entities.Item;
+import com.example.noteapp.entities.Note;
+import com.example.noteapp.services.ItemService;
+import com.example.noteapp.services.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/items")
 public class ItemController {
-    private ItemRepository itemRepository;
+    private ItemService itemService;
+    private NoteService noteService;
 
     @Autowired
-    public ItemController(ItemRepository itemRepository) {
-        this.itemRepository = itemRepository;
+    public ItemController(ItemService itemService, NoteService noteService) {
+        this.itemService = itemService;
+        this.noteService = noteService;
+    }
+
+    @GetMapping("/{noteId}")
+    public ResponseEntity<?> getItem(@PathVariable int noteId) {
+        try {
+            Note note = noteService.getNoteById(noteId);
+            return ResponseEntity.ok(itemService.getItemsByNote(note));
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body(exception.getMessage());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createItem(@RequestBody Item item) {
+        try {
+            return ResponseEntity.ok(itemService.createOrUpdateItem(item));
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body(exception.getMessage());
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateItem(@RequestBody Item item) {
+        try {
+            return ResponseEntity.ok(itemService.createOrUpdateItem(item));
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body(exception.getMessage());
+        }
     }
 }
